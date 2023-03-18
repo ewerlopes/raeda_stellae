e9de111c1b403bd22302c79ec0bb9118c066a3bd
ewerlopes <ewerlopes@gmail.com>
2023-03-18 18:40:48 -0300

    update credentials.

--- telebot/credentials.py
+++ telebot/credentials.py
@@ -1,3 +1,3 @@
 bot_token = "6264079997:AAE2B4ghEjcn9vGW2ZrLWUT2FHG2AXVVNhA"
 bot_user_name = "raedastellae_bot"
-URL = "https://raedastellae-ewerlopes.b4a.run"
+URL = "https://raedastellae-ewerlopes.b4a.run/"
