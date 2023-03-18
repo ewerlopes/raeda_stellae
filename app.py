c339450d61663589a8b2c840aa9550060db92f35
ewerlopes <ewerlopes@gmail.com>
2023-03-18 14:21:00 -0300

    updates requirements.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 import telegram
-from telebot.credentials import bot_token, bot_user_name,URL
+from telebot.credentials import bot_token, bot_user_name, URL
 import re
 
 global bot
